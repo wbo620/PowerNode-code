b9b589f8f9f2756c3ae6c23a7e8cfa8cde9ebc82
wbo620 <468308027@qq.com>
2023-08-22 20:08:35 +0800

    异常处理

---
+++ springboot2/lesson16-excpetionHandle/src/main/java/com/powernode/exp/Lesson16ExcpetionHandleApplication.java
@@ -0,0 +1,13 @@
+package com.powernode.exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson16ExcpetionHandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson16ExcpetionHandleApplication.class, args);
+	}
+
+}
---
+++ springboot2/lesson16-excpetionHandle/src/test/java/com/powernode/exp/Lesson16ExcpetionHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powernode.exp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson16ExcpetionHandleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
