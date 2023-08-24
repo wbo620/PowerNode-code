44c49bbd3c816ae459cd72cd9ddf6d6e68abfef2
wbo620 <468308027@qq.com>
2023-08-24 17:45:00 +0800

    使用problemDetai处理异常

---
+++ springboot2/lesson17-problemDetail/src/main/java/com/powernode/problem/Lesson17ProblemDetailApplication.java
@@ -0,0 +1,19 @@
+package com.powernode.problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 这个懒了,没写,
+ * 视频地址
+ * 【【动力节点】SpringBoot3从入门到项目实战-spring boot3通俗易懂高薪必备】
+ * https://www.bilibili.com/video/BV1Km4y1k7bn?p=160&vd_source=9754d787a7e94230ea564beefa400978
+ */
+@SpringBootApplication
+public class Lesson17ProblemDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson17ProblemDetailApplication.class, args);
+	}
+
+}
---
+++ springboot2/lesson17-problemDetail/src/test/java/com/powernode/problem/Lesson17ProblemDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powernode.problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson17ProblemDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
